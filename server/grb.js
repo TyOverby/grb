c7734771783db10174e7b6c19e27d5f516fe743e
Meyer Kizner <meyer@pre-alpha.com>
2014-09-14 01:23:18 -0400

    Rework the main server file so that other things are possible

--- server/grb.js
+++ server/grb.js
@@ -1,7 +1,7 @@
 /* jshint node: true */
 'use strict';
 
-var socketio = require('socket.io');
+var Server = require('socket.io');
 
 var Instance = require('./Instance.js');
 
@@ -11,14 +11,14 @@ var facets = {
   BroadcastFacet: require('./BroadcastFacet.js')
 };
 
-var serve = function (path, namespace, id, facets) {
-  var io = socketio();
+var serve = function (server, path, namespace, id, facets) {
+  var io = new Server(server);
   var instance = new Instance(namespace, id, facets);
-  io.of(path).use(function (socket) {
-    socket.on('connection', instance._onConnection);
-    socket.on('disconnect', instance._onDisconnection);
-    socket.on('load', instance._onLoad);
-    socket.on('delta', instance._onDelta);
+  io.of(path).on('connection', function (socket) {
+    instance._onConnection(socket);
+    socket.on('disconnect', instance._onDisconnection.bind(instance));
+    socket.on('load', instance._onLoad.bind(instance));
+    socket.on('delta', instance._onDelta.bind(instance));
   });
 };
 
